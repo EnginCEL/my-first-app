fed82e9a62a027cab4805d2af0fe641802d92103
Engin ÇELİK <80245505+EnginCEL@users.noreply.github.com>
2022-11-07 16:09:53 +0300

    Disabled Button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-first-app';
+export class AppComponent  implements  OnInit{
+  name = 'my-first-app';
+allowNewServe=true;
+
+  constructor(){
+setTimeout(()=>{
+  this.allowNewServe=false;
+},3000)
+  }
+ngOnInit() {
+      }
 }
